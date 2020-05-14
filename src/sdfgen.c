dd39b293b3f69114d10eb886d39c4276d78757fb
preversewharf45 <preverse45you@gmail.com>
2020-05-14 16:56:15 +0530

    added few structures and helper functions

--- src/sdfgen.c
+++ src/sdfgen.c
@@ -1,14 +1,82 @@
 
 #include <ft2build.h>
+#include FT_INTERNAL_DEBUG_H
+
 #include FT_FREETYPE_H
 #include FT_ERRORS_H
+#include FT_INTERNAL_OBJECTS_H
 
 #include "sdfgen.h"
 
-  FT_EXPORT_DEF(FT_Error)
+  FT_EXPORT_DEF( FT_Error )
   FT_Generate_SDF( FT_Library   library,
                    FT_Outline*  outline, 
                    FT_Bitmap   *abitmap )
   {
-    return FT_Err_Unimplemented_Feature;
+    
+    
+    
+    return FT_THROW( Unimplemented_Feature );
   }
+
+
+  /**************************************************************************
+   *
+   * functions.
+   *
+   */
+
+  static
+  const SDF_Contour null_sdf_contour = { { 0.0f, 0.0f }, { 0.0f, 0.0f },
+                                         { 0.0f, 0.0f }, { 0.0f, 0.0f },
+                                         SDF_CONTOUR_TYPE_NONE, NULL };
+
+  static
+  const SDF_Shape null_sdf_shape = { { 0.0f, 0.0f }, NULL, 0u };
+
+  FT_LOCAL_DEF( void )
+  SDF_Contour_Init( SDF_Contour  *contour )
+  {
+    if ( contour )
+      *contour = null_sdf_contour;
+  }
+
+  FT_LOCAL_DEF( void )
+  SDF_Shape_Init( SDF_Shape  *shape )
+  {
+    if ( shape )
+      *shape = null_sdf_shape;
+  }
+
+  FT_LOCAL_DEF( FT_Error )
+  SDF_Shape_Done( FT_Library  library,
+                  SDF_Shape  *shape )
+  {
+    FT_Memory memory;
+
+
+    if ( !library )
+      return FT_THROW( Invalid_Library_Handle) ;
+
+    if ( !shape )
+      return FT_THROW( Invalid_Argument );
+
+    memory = library->memory;
+    
+    SDF_Contour * head = shape->contour_head;
+
+    while ( head )
+    {
+      SDF_Contour * temp = head;
+      head = head->next;
+
+      FT_FREE( temp );
+    }
+
+    *shape = null_sdf_shape;
+
+    return FT_Err_Ok;
+  }
+  
+
+/* END */
--- src/sdfgen.h
+++ src/sdfgen.h
@@ -8,16 +8,24 @@
 
 FT_BEGIN_HEADER
 
-  // generate sdf from outline
-  FT_EXPORT(FT_Error)
+  /* generate sdf from outline */
+  /* input: library, outline   */
+  /* output: abitmap           */
+  FT_EXPORT( FT_Error )
   FT_Generate_SDF( FT_Library   library,
                    FT_Outline*  outline, 
                    FT_Bitmap   *abitmap );
 
+  
+  /* Private Stuff */
 
-  /* ------------------- private stuff ------------------- */
+  /**************************************************************************
+   *
+   * SDF Outline implementation structs and enums.
+   *
+   */
 
-  // float vector
+  /* float vector */
   typedef struct  SDF_Vector_
   {
     float  x;
@@ -28,6 +36,7 @@ FT_BEGIN_HEADER
   /* enumeration for the types of contour present in FT_Outline */
   typedef enum  SDF_Contour_Type_
   {
+    SDF_CONTOUR_TYPE_NONE,
     SDF_CONTOUR_TYPE_LINE,              /* line type contour */
     SDF_CONTOUR_TYPE_QUADRATIC_BEZIER,  /* quadratic bezier type contour */
     SDF_CONTOUR_TYPE_CUBIC_BEZIER,      /* cubic bezier type contour */
@@ -39,15 +48,47 @@ FT_BEGIN_HEADER
   /* structure to hold a contour */
   typedef struct  SDF_Contour_
   {
-    SDF_Vector  start_pos;         /* all contour type has a start position */
-    SDF_Vector  end_pos;           /* all contour type has a end position */
+    SDF_Vector  start_pos;          /* all contour type has a start position */
+    SDF_Vector  end_pos;            /* all contour type has a end position */
+                                    
+    SDF_Vector  control_point_a;    /* unused in line type contour */
+    SDF_Vector  control_point_b;    /* unused in line and quadratic bezier type contour */
 
-    SDF_Vector  control_point_a;   /* unused in line type contour */
-    SDF_Vector  control_point_b;   /* unused in line and quadratic bezier type contour */
-
-    SDF_Contour_Type contour_type  /* contour identifier */
+    SDF_Contour_Type contour_type;  /* contour identifier */
+    
+    struct SDF_Contour_* next;     /* used to create a linked list of contour */
 
   } SDF_Contour;
+
+  /* structure represent a complete shape defined by FT_Outline */
+  typedef struct SDF_Shape_
+  {
+    SDF_Vector current_pos;      /* used to store move_to position while decomposing FT_Outline */
+
+    SDF_Contour* contour_head;  /* linked list of all the contours present in the FT_Outline */
+
+    FT_ULong num_contours;       /* number of contours present in the contour linked list */
+
+  } SDF_Shape;
+
+
+  /**************************************************************************
+   *
+   * SDF Outline implementation functions.
+   *
+   */
+
+  FT_LOCAL( void )
+  SDF_Contour_Init( SDF_Contour  *contour );
+
+  FT_LOCAL( void )
+  SDF_Shape_Init( SDF_Shape  *shape );
+
+  /* no need to create SDF_Contour_Done becuase            */
+  /* SDF_Shape_Done will free all the created SDF_Contour  */
+  FT_LOCAL( FT_Error )
+  SDF_Shape_Done( FT_Library  library,
+                  SDF_Shape  *shape );
 
 FT_END_HEADER
 
